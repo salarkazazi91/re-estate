68812d36b3c487ad7d7b09b6eea09b57888ea651
salar.kazazi <salarkazazi91@gmail.com>
2022-04-29 06:18:25 +0430

    close menu click outside phase 1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -24,10 +24,21 @@ export class HomeComponent implements OnInit {
   ]
   states = ['آپارتمان', 'ویلا و باغ', 'زمین و کلنگی', 'اداری', 'تجاری', 'مستغلات'];
 
+  @ViewChild('melkElement') melkElement!: ElementRef;
+  @ViewChild('melkBtn') melkBtn!: ElementRef;
+
   // FUNCTIONS
 
 
-  constructor() { }
+  constructor(private renderer: Renderer2) {
+    this.renderer.listen('window', 'click', (e: Event) => {
+      if (e.target !== this.melkElement.nativeElement && e.target !== this.melkBtn.nativeElement
+        && !Array.from(this.melkElement.nativeElement.children).find(x => x == e.target)) {
+         this.openSearchControl = '';
+      }
+    })
+
+  }
 
   ngOnInit(): void {
   }
@@ -37,7 +48,7 @@ export class HomeComponent implements OnInit {
     this.tags.splice(this.tags.indexOf(tag), 1)
   }
 
-  searchContollClicked(control:string) {
+  searchContollClicked(control: string) {
     this.openSearchControl = this.openSearchControl == control ? '' : control;
   }
 
