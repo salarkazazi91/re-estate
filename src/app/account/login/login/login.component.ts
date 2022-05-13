96934f35a0f1bd984df1b90af2214ca3e10c2170
salar.kazazi <salarkazazi91@gmail.com>
2022-05-13 21:10:51 +0430

    login form

--- src/app/account/login/login/login.component.ts
+++ src/app/account/login/login/login.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  // VARS
 
-  constructor() { }
+  formGroup = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
 
-  ngOnInit(): void {
+  // METHODS
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  submitOnEnter(e: any) {
+    if (e.code.toString().toLowerCase() === 'enter') this.submit();
+    return;
   }
 
+  submit() {
+    console.log(this.formGroup);
+  }
 }
